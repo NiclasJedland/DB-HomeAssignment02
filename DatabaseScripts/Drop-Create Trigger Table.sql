d17ef044470b1664aca8ce2539a6ae0bbecf5557
Niclas Jedland <jedland82@hotmail.com>
2016-11-02 22:39:33 +0100

    Added Trigger and Drop/Create-Table-script

---
+++ DatabaseScripts/Drop-Create Trigger Table.sql
@@ -0,0 +1,28 @@
+﻿USE [NORTHWND]
+GO
+
+/****** Object:  Table [dbo].[ContactNameChange]    Script Date: 2016-11-02 22:30:59 ******/
+DROP TABLE [dbo].[ContactNameChange]
+GO
+
+/****** Object:  Table [dbo].[ContactNameChange]    Script Date: 2016-11-02 22:30:59 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ContactNameChange](
+	[NameChangeID] [int] IDENTITY(1,1) NOT NULL,
+	[OldContactName] [nvarchar](30) NOT NULL,
+	[NewContactName] [nvarchar](30) NOT NULL,
+	[TimeOfUpdate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ContactNameChange] PRIMARY KEY CLUSTERED 
+(
+	[NameChangeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
---
+++ DatabaseScripts/TriggerUpdateName.sql
@@ -0,0 +1,26 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Niclas
+-- Create date: 
+-- Description:	
+-- =============================================
+CREATE TRIGGER TriggerUpdateName 
+   ON Customers 
+   AFTER UPDATE
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for trigger here
+	INSERT INTO ContactNameChange ([OldContactName],[NewContactName], [TimeOfUpdate])
+		SELECT d.ContactName, i.ContactName, GETDATE()
+			FROM inserted i
+			INNER JOIN deleted d ON i.CustomerID = d.CustomerID
+
+END
+GO
