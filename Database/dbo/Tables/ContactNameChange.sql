6435b786db865038802690df205bbfaf4bb2b4a0
Niclas Jedland <niclasjedland@gmail.com>
2016-11-09 12:12:29 +0100

    Trigger

---
+++ Database/dbo/Tables/ContactNameChange.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ContactNameChange]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [OldContactName] NVARCHAR(50) NOT NULL, 
+    [NewContactName] NVARCHAR(50) NOT NULL, 
+    [TimeOfUpdate] DATETIME NOT NULL
+)
