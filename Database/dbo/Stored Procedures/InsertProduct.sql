72fcd804a9c6e100529c52f9d7416f471227ec26
Niclas Jedland <niclasjedland@gmail.com>
2016-11-09 11:14:41 +0100

    InsertProduct added

--- Database/dbo/Stored Procedures/InsertProduct.sql
+++ Database/dbo/Stored Procedures/InsertProduct.sql
@@ -1,5 +1,4 @@
-﻿
-CREATE PROCEDURE [dbo].[InsertProduct] (@ProductName nvarchar(40), @SupplierID int, @CategoryID int, @QuantityPerUnit nvarchar(20), @UnitPrice money, @UnitsInStock int, @UnitsOnOrder int, @ReorderLevel int, @Discontinued bit)
+﻿CREATE PROCEDURE [dbo].[InsertProduct] (@ProductName nvarchar(40), @SupplierID int, @CategoryID int, @QuantityPerUnit nvarchar(20), @UnitPrice money, @UnitsInStock int, @UnitsOnOrder int, @ReorderLevel int, @Discontinued bit)
 AS
 
 INSERT INTO [dbo].[Products](ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued)
